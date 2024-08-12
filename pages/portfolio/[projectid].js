6660516b78d3d796de31912f0cc8d39424c8f1ef
Deepu Cyriac <deepucvk123@gmail.com>
2024-08-12 07:23:51 +0530

    Added dynamic route

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,18 @@
+//this is a dynamic route.
+
+import { useRouter } from "next/router";
+
+export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  console.log(router.pathname);
+  console.log(router.query);
+
+  // send a request to backend server to fetch
+  // data with an id of router.query.projectid
+  return (
+    <div>
+      <h1>The Portfolio Project Page</h1>
+    </div>
+  );
+}
